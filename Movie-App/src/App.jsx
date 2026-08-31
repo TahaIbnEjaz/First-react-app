82ef1947cfd7d0d7257a7961266fbe930a485dce
muhammadtaha <muhammadtahakarachi220@gmail.com>
2026-08-31 13:03:20 +0500

    error

--- Movie-App/src/App.jsx
+++ Movie-App/src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import Search from './components/Search.jsx'
 
 const App = () => {
+
+  const[searchTerm,setSearchTerm] = useState('Khaddi Kabab');
+
   return (
-    <div>
-      
-    </div>
+    
+    <main>
+
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+
+          <img src="./src/assets/hero.png" alt="Hero Banner"/>
+          <h1 className=""> Find <span className="text-gradient">Movies</span> You'll Enjoy Without Any Hassle</h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+
+    </main>
+
   )
 }
 
---
+++ Movie-App/src/components/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      {searchTerm}
+    </div>
+  )
+}
+
+export default Search
